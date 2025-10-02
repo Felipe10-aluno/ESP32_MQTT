1ecfa638df2d8287e56410359fa180f0191755d0
FELIPE DE LIMA PEREIRA <felipe_lima-pereira@estudante.sesisenai.org.br>
2025-10-02 14:06:22 -0300

    basic-mqtt

---
+++ basic-mqtt/basic-mqtt.ino
@@ -0,0 +1,74 @@
+#include <WiFi.h> 
+#include <PubSubClient.h>
+
+const String SSID = "A55 de Isaque";
+const String PSWD = "12345678";
+
+const String brokerUrl = "test.mosquitto.org";         // URL do broker (servidor)
+const int port = 1883;                                // Porta do Broker (servidor)
+
+WiFiClient espClient;                                // Criando Cliente WiFi
+PubSubClient mqttClient(espClient)                  // Criando Cliente MQTT
+
+void scanLocalNetworks();
+
+void conexaoWifi(){
+  WiFi.begin(SSID,PSWD);
+  Serial.println("Iniciando conexão com rede Wi-Fi");
+  Serial.print("Conectando");
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.println("\nConectado!");
+}
+
+void reconexaoWifi(){
+  
+  Serial.println("Iniciando reconexão com rede Wi-Fi");
+  Serial.print("Reconectando");
+  while(WiFi.status() != WL_CONNECTED){
+    WiFi.begin(SSID,PSWD);
+    Serial.print(".");
+    delay(2000);
+  }
+  Serial.println("\nReconectado!");
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  connectToWifi()
+  scanLocalNetworks();
+  conexaoWifi();
+  mqttClient.setServer(brokerUrl.c_str(), port);
+  String userId = "ESP-FELIPE";
+  mqttClient.connect(UserId.c_str());
+  while(!mqttClient.connected()){
+    Serial.println("Erro de conexão");
+    delay(500);
+      }
+}
+
+void loop() {
+  if(WiFi.status() != WL_CONNECTED){
+    reconexaoWifi();
+  }
+  mqttClient.loop()
+}
+
+void scanLocalNetworks(){
+  Serial.println("Iniciando o Scan de redes Wi-Fi");
+  int number = WiFi.scanNetworks();
+    delay(500);
+    if(number == -1){
+      Serial.println("ERRO!, problema em encontrar alguma rede");
+    }
+    else{
+      for(int net = 0; net < net; net++){
+        Serial.printf("%d - %s | %d db\n",net,WiFi.SSID(net),WiFi.RSSI(net));
+      }
+      Serial.printf("Número de redes encontradas: %d\n", number);
+
+    }
+}
